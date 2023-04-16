da711dc9e711a05fd10930351df08674380ee66d
releaser <herminiohenriques@gmail.com>
2023-04-16 23:07:47 +0100

    Article detail component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import Homepage from "./pages/home/Homepage";
 
+import { Routes, Route } from "react-router-dom";
+
 import "./App.css";
+import ArticleDetailPage from "./pages/articleDetail/ArticleDetail";
 
 function App() {
   return (
     <div className="App font-opensans">
-      <Homepage />
+      <Routes>
+        <Route index path="/" element={<Homepage />} />
+        <Route path="/blog/:id" element={<ArticleDetailPage />} />
+      </Routes>
     </div>
   );
 }
